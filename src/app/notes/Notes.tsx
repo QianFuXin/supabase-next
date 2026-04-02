21ce4ba32f093e14deffa4df47a131b641ad27ba
QianFuXin <mr_qian_ives@163.com>
2026-04-02 22:56:10 +0800

    add Suspense

---
+++ src/app/notes/Notes.tsx
@@ -0,0 +1,9 @@
+import { createClient } from '@/supabase/server'
+
+// src/app/notes/Notes.tsx
+export default async function Notes() {
+  const supabase = await createClient()
+  const { data: notes } = await supabase.from('notes').select()
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+}
--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,8 +1,10 @@
-import { createClient } from '@/supabase/server'
+import { Suspense } from 'react'
+import Notes from './Notes'
 
-export default async function Page() {
-  const supabase = await createClient()
-  const { data: notes } = await supabase.from('notes').select()
-
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Notes />
+    </Suspense>
+  )
 }
